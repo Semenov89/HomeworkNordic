e32d55cf5105eac2e3ed25d95952e4cd57135dd5
Semenov89 <102901916+Semenov89@users.noreply.github.com>
2022-06-22 17:59:12 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Chess
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // константа, задающая размер поля
+            int size = 8;
+
+            // формирование поля
+            for (int i = 1; i <=size; i++)
+            {
+                for (int j = 1; j <=size/2; j++)
+                {
+                    if (i % 2 == 1)
+                    {
+                        Console.BackgroundColor = ConsoleColor.White;
+                        Console.Write("  ");
+                        Console.BackgroundColor = ConsoleColor.Black;
+                        Console.Write("  ");
+                    }
+                    else
+                    {
+                        Console.BackgroundColor = ConsoleColor.Black;
+                        Console.Write("  ");
+                        Console.BackgroundColor = ConsoleColor.White;
+                        Console.Write("  ");
+                    }
+                }
+                Console.WriteLine();
+
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
+
+
+
